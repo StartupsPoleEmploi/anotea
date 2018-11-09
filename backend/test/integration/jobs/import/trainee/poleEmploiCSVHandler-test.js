d7eb7ffd9457ed85888b11c3bb5b87681decb339
Benoît Guérout <bguerout@yonoma.com>
2018-11-09 16:41:42 +0100

    Fix tests to check that same trainee cannot be inserted twice

--- backend/test/integration/jobs/import/trainee/poleEmploiCSVHandler-test.js
+++ backend/test/integration/jobs/import/trainee/poleEmploiCSVHandler-test.js
@@ -207,13 +207,16 @@ describe(__filename, withMongoDB(({ getTestDatabase, insertIntoDatabase }) => {
         let csvFileWithDuplicates = path.join(__dirname, '../../../../helpers/data', 'stagiaires-pe-doublons.csv');
         let importer = traineeImporter(db, logger, configuration);
         let handler = poleEmploiCSVHandler(db, logger, configuration);
+        await addIleDeFranceRegion();
+
         await importer.importTrainee(csvFile, handler);
         let results = await importer.importTrainee(csvFileWithDuplicates, handler);
+
         assert.deepEqual(results, {
-            invalid: 2,
+            invalid: 0,
             ignored: 1,
             imported: 0,
-            total: 3,
+            total: 1,
         });
     });
 
