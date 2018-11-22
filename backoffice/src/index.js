4b3b75ece966f90b562c53fe7386fd175c7a3bfd
Loïc Guillois <contact@loicguillois.fr>
2018-11-22 11:47:34 +0100

    Fix popper/bootstrap JS integration

--- backoffice/src/index.js
+++ backoffice/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 import App from './App';
 import './styles/index.css';
 
