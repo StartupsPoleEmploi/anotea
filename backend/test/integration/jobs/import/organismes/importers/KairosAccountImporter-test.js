974cd614f9312d6100d395925f06fc8ab01f499d
Benoît Guérout <bguerout@yonoma.com>
2018-12-05 15:08:20 +0100

    Fix case sensitive issue in class names

--- backend/test/integration/jobs/import/organismes/importers/KairosAccountImporter-test.js
+++ backend/test/integration/jobs/import/organismes/importers/KairosAccountImporter-test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const { withMongoDB } = require('../../../../../helpers/test-db');
 const { newOrganismeAccount, newComment } = require('../../../../../helpers/data/dataset');
 const logger = require('../../../../../helpers/test-logger');
-const KairosAccountImporter = require('../../../../../../jobs/import/organismes/importers/KairosAccountImporter');
+const KairosAccountImporter = require('../../../../../../jobs/import/organismes/accounts/KairosAccountImporter');
 
 describe(__filename, withMongoDB(({ getTestDatabase, insertIntoDatabase }) => {
 
