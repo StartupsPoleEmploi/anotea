6bf35488ec12d8407ffb042e7b5f504678311dfb
Benoît Guérout <bguerout@yonoma.com>
2018-12-07 14:43:06 +0100

    Use cursor.batchSize(10); in all mailing scripts

--- backend/jobs/mailing/organismes/notifications/NotificationMailer.js
+++ backend/jobs/mailing/organismes/notifications/NotificationMailer.js
@@ -103,6 +103,7 @@ class NotificationMailer {
         if (options.limit) {
             cursor.limit(options.limit);
         }
+        cursor.batchSize(10);
 
         while (await cursor.hasNext()) {
             let { organisme, status } = await cursor.next();
