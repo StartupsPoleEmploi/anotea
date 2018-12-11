3605222767a7584aee0481cde6197d6f3d89aca1
Benoît Guérout <bguerout@yonoma.com>
2018-12-11 09:28:44 +0100

    Generate CSV for all active regions

--- backend/jobs/stats/buildOrganismesCSV.js
+++ backend/jobs/stats/buildOrganismesCSV.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const { encodeStream } = require('iconv-lite');
 const path = require('path');
 const moment = require('moment');
-const cli = require('commander');
 const configuration = require('config');
 const getMongoClient = require('../../components/mongodb');
 const getLogger = require('../../components/logger');
@@ -17,44 +16,49 @@ const main = async () => {
     let client = await getMongoClient(configuration.mongodb.uri);
     let db = client.db();
     let logger = getLogger('anotea-job-stats-mail-domain', configuration);
-    let abort = message => {
+
+    const abort = message => {
         logger.error(message, () => {
             client.close(() => process.exit(1));
         });
     };
 
-    cli.option('-r, --region [region]')
-    .option('-o, --output [output]')
-    .parse(process.argv);
+    const generateCSV = codeRegion => {
+        return new Promise((resolve, reject) => {
+            let total = 0;
+            let output = fs.createWriteStream(path.join(__dirname, `../../../.data/organismes-${codeRegion}.csv`));
 
-    let region = cli.region;
-    if (!region) {
-        return abort('Region is required');
-    }
+            output.write(`Siret;Raison sociale;Email;Nombre Avis\n`);
+
+            db.collection('organismes').find({ codeRegion }).transformStream({
+                transform: organisme => {
+                    return `="${organisme.meta.siretAsString}";"${organisme.raisonSociale}";"${getContactEmail(organisme)}";"${organisme.meta.nbAvis || 0}"\n`;
+                }
+            })
+            .on('data', () => total++)
+            .pipe(encodeStream('UTF-16BE'))
+            .pipe(output)
+            .on('error', e => reject(e))
+            .on('finish', async () => {
+                logger.info(`Results: ${total} organismes for region ${codeRegion}`);
+                resolve();
+            });
+        });
+    };
 
     logger.info(`Generating CSV file...`);
 
-    let total = 0;
-    let output = fs.createWriteStream(cli.output || path.join(__dirname, `../../../.data/organismes-${region}.csv`));
-
-    output.write(`Siret;Raison sociale;Email;Nombre Avis\n`);
-
-    db.collection('organismes').find({ codeRegion: region }).transformStream({
-        transform: organisme => {
-            return `="${organisme.meta.siretAsString}";"${organisme.raisonSociale}";"${getContactEmail(organisme)}";"${organisme.meta.nbAvis || 0}"\n`;
-        }
-    })
-    .on('data', () => total++)
-    .pipe(encodeStream('UTF-16BE'))
-    .pipe(output)
-    .on('error', e => abort(e))
-    .on('finish', async () => {
+    try {
+        let activeRegions = configuration.app.active_regions.map(region => region.code_region);
+        await Promise.all(activeRegions.map(codeRegion => generateCSV(codeRegion)));
         await client.close();
 
         let duration = moment.utc(new Date().getTime() - launchTime).format('HH:mm:ss.SSS');
         logger.info(`Completed in ${duration}`);
-        logger.info(`Results: ${total} organismes for region ${region}`);
-    });
+
+    } catch (e) {
+        abort(e);
+    }
 };
 
 main();
