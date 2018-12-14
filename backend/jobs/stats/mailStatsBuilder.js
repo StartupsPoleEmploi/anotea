b37244343b2ddd9bfe6c4e678763ae3763815d49
Loïc Guillois <contact@loicguillois.fr>
2018-12-14 11:49:09 +0100

    wip

--- backend/jobs/stats/mailStatsBuilder.js
+++ backend/jobs/stats/mailStatsBuilder.js
@@ -8,7 +8,7 @@ module.exports = (db, logger) => {
         let launchTime = new Date().getTime();
 
         await db.collection('trainee').aggregate([
-            { $match: { 'mailSentDate': { $ne: null } } },
+            { $match: { 'mailSentDate': { $gt: new Date(2018,9,9) } } }, // $ne : null
             { $unwind: '$training.codeFinanceur' },
             {
                 $lookup: {
@@ -23,6 +23,71 @@ module.exports = (db, logger) => {
                     mailSentDate: '$mailSentDate',
                     codeRegion: '$codeRegion',
                     codeFinanceur: '$training.codeFinanceur',
+                    count: { '$size': '$advices' },
+                    emailOpen: {
+                        $filter: {
+                            input: '$advices',
+                            as: 'advice',
+                            cond: { $gte: ['$$advice.step', 1] }
+                        }
+                    },
+                    advicesPublished: {
+                        $filter: {
+                            input: '$advices',
+                            as: 'advice',
+                            cond: { $gte: ['$$advice.step', 2] } // TODO : published
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    mailSentDate: '$mailSentDate',
+                    codeRegion: '$codeRegion',
+                    codeFinanceur: '$codeFinanceur',
+                    count: '$count',
+                    countEmailOpen: {
+                        '$size': '$emailOpen'
+                    },
+                    countAdvicesPublished: {
+                        '$size': '$advicesPublished'
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        year: { $year: '$mailSentDate' },
+                        month: { $month: '$mailSentDate' },
+                        codeRegion: '$codeRegion',
+                        codeFinanceur: '$codeFinanceur'
+                    },
+                    count: { $sum: '$count' },
+                    countEmailOpen: { $sum: '$countEmailOpen' },
+                    countAdvicesPublished: { $sum: '$countAdvicesPublished' }
+                },
+            } /*,{ $project: {
+                mailSentDate: '$mailSentDate',
+                codeRegion: '$codeRegion',
+                codeFinanceur: '$training.codeFinanceur',
+                count: '$count',
+                emailOpen: {
+                    $filter: {
+                        input: '$advices',
+                        as: 'advice',
+                        cond: { eq: ['$$advice.step', 1] }
+                    }
+                }
+            } }*/, {
+                $sort: { '_id.year': 1, '_id.month': 1 }
+            },
+            { $out: 'mailStats' }
+        ], { allowDiskUse: true }).toArray();/*, {
+                $project: {
+                    mailSentDate: '$mailSentDate',
+                    codeRegion: '$codeRegion',
+                    codeFinanceur: '$training.codeFinanceur',
+                    advices: '$advices',
                     emailOpen: {
                         $filter: {
                             input: '$advices',
@@ -30,19 +95,21 @@ module.exports = (db, logger) => {
                             cond: { eq: ['$$advice.step', 1] }
                         }
                     },
-                    advices: {
+                    advicesPublished: {
                         $filter: {
                             input: '$advices',
                             as: 'advice',
-                            cond: {
-                                $and: [
-                                    { published: true },
-                                    { eq: ['$$advice.step', 2] }
-                                ]
-                            }
+                            cond: { eq: ['$$advice.step', 2] }
                         }
                     },
-                    advicesWithComments: {
+                }},
+                {
+                    $sort: { '_id.year': 1, '_id.month': 1 }
+                },
+                { $out: 'mailStats' }
+            ]).toArray();
+
+                   /* advicesWithComments: {
                         $filter: {
                             input: '$advices',
                             as: 'advice',
@@ -90,10 +157,8 @@ module.exports = (db, logger) => {
                                 ]
                             }
                         }
-                    },
-                }
-            },
-            {
+                    },*/
+          /*  {
                 $group: {
                     _id: {
                         year: { $year: '$mailSentDate' },
@@ -102,19 +167,16 @@ module.exports = (db, logger) => {
                         codeFinanceur: '$codeFinanceur'
                     },
                     count: { $sum: 1 },
+                    advices: { $first: '$advices' }
+                  /*  countAll: { $sum: { $size: '$advices' } },
                     countOpen: { $sum: { $size: '$emailOpen' } },
-                    countAdvices: { $sum: { $size: '$advices' } },
-                    countAdvicesWithComments: { $sum: { $size: '$advicesWithComments' } },
+                    countAdvices: { $sum: { $size: '$advicesPublished' } },
+                    /*countAdvicesWithComments: { $sum: { $size: '$advicesWithComments' } },
                     countAdvicesPositif: { $sum: { $size: '$advicesPositif' } },
                     countAdvicesNegatif: { $sum: { $size: '$advicesNegatif' } },
                     countAdvicesRejected: { $sum: { $size: '$advicesRejected' } }
                 }
-            },
-            {
-                $sort: { '_id.year': 1, '_id.month': 1 }
-            },
-            { $out: 'mailStats' }
-        ]).toArray();
+            },*/
 
         logger.info(`Build email statistics displayed on financer dashboard - completed (${moment.utc(new Date().getTime() - launchTime).format('HH:mm:ss.SSS')})`);
     };
