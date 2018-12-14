6d6227141918185b9fc4ede157adc526c94b179b
Loïc Guillois <contact@loicguillois.fr>
2018-12-14 11:49:09 +0100

    wip

--- backend/routes/backoffice/stats.js
+++ backend/routes/backoffice/stats.js
@@ -6,7 +6,7 @@ module.exports = (db, authService, logger, configuration) => {
     const router = express.Router(); // eslint-disable-line new-cap
     const checkAuth = authService.createJWTAuthMiddleware('backoffice');
 
-    router.get('/backoffice/financeur/region/:idregion/mailStats/:year', checkAuth, tryAndCatch(async (req, res) => {
+    router.get('/backoffice/financeur/region/:idregion/mailStats/:year/months', tryAndCatch(async (req, res) => {
 
         let codeFinanceur = req.query.codeFinanceur;
 
@@ -23,16 +23,48 @@ module.exports = (db, authService, logger, configuration) => {
                 {
                     _id: '$_id.month',
                     count: { $sum: '$count' },
-                    countOpen: { $sum: '$count' },
-                    countAdvices: { $sum: '$count' },
-                    countAdvicesPositif: { $sum: '$count' },
-                    countAdvicesNegatif: { $sum: '$count' },
-                    countAdvicesRejected: { $sum: '$count' }
+                    countOpen: { $sum: '$countOpen' },
+                    countAdvices: { $sum: '$countAdvices' },
+                    countAdvicesPositif: { $sum: '$countAdvicesPositif' },
+                    countAdvicesNegatif: { $sum: '$countAdvicesNegatif' },
+                    countAdvicesRejected: { $sum: '$countAdvicesRejected' }
                 }
             }
         ]).toArray();
         
         res.status(200).send(stats);
+    }));
+
+    router.get('/backoffice/financeur/region/:idregion/mailStats/:year', tryAndCatch(async (req, res) => {
+
+        let codeFinanceur = req.query.codeFinanceur;
+
+        let match = { '_id.codeRegion': req.params.idregion, '_id.year': parseInt(req.params.year) };
+        if (codeFinanceur) {
+            match = Object.assign(match, { '_id.codeFinanceur': codeFinanceur });
+        }
+
+        let stats = await db.collection('mailStats').aggregate([
+            { $match:
+                match
+            },
+            { $group:
+                {
+                    _id: null,
+                    count: { $sum: '$count' },
+                    countOpen: { $sum: '$countOpen' },
+                    countAdvices: { $sum: '$countAdvices' },
+                    countAdvicesPositif: { $sum: '$countAdvicesPositif' },
+                    countAdvicesNegatif: { $sum: '$countAdvicesNegatif' },
+                    countAdvicesRejected: { $sum: '$countAdvicesRejected' }
+                }
+            }
+        ]).toArray();
+
+        let obj = stats[0];
+        delete obj._id;
+        
+        res.status(200).send(obj);
     }));
 
     return router;
