9d62960659a2eb502d1dc6d3765c1e5f62422c3f
Loïc Guillois <contact@loicguillois.fr>
2018-12-17 14:09:58 +0100

    enable stats

--- backend/routes/backoffice/stats.js
+++ backend/routes/backoffice/stats.js
@@ -6,7 +6,7 @@ module.exports = ({ db, authService }) => {
     const router = express.Router(); // eslint-disable-line new-cap
     const checkAuth = authService.createJWTAuthMiddleware('backoffice');
 
-    router.get('/backoffice/financeur/region/:idregion/mailStats/:year/months', tryAndCatch(async (req, res) => {
+    router.get('/backoffice/financeur/region/:idregion/mailStats/:year/months', checkAuth, tryAndCatch(async (req, res) => {
 
         let codeFinanceur = req.query.codeFinanceur;
 
@@ -36,7 +36,7 @@ module.exports = ({ db, authService }) => {
         res.status(200).send(stats);
     }));
 
-    router.get('/backoffice/financeur/region/:idregion/mailStats/:year', tryAndCatch(async (req, res) => {
+    router.get('/backoffice/financeur/region/:idregion/mailStats/:year', checkAuth, tryAndCatch(async (req, res) => {
 
         let codeFinanceur = req.query.codeFinanceur;
 
