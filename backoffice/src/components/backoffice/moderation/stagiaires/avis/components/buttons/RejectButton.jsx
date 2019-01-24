f5aa10c8678e4f9ad06031786db704554268b997
nchoukri <nabil.tri@gmail.com>
2019-01-24 12:21:11 +0100

    Use the Modal to show a confirmation message to the moderators when a comment is qualified as 'injure'

--- backoffice/src/components/backoffice/moderation/stagiaires/avis/components/buttons/RejectButton.jsx
+++ backoffice/src/components/backoffice/moderation/stagiaires/avis/components/buttons/RejectButton.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { rejectAvis } from '../../../../../../../lib/avisService';
 import './RejectButton.scss';
-import InjureTagModal from '../InjureTagModal';
+import Modal from '../../../../../common/Modal';
 
 export default class RejectButton extends React.Component {
 
     state = {
-        modalIsOpen: false,
+        showModal: false,
     };
 
     static propTypes = {
@@ -17,19 +17,27 @@ export default class RejectButton extends React.Component {
     };
 
     reject = async (avis, reason) => {
+        this.setState({ showModal: false });
         let updated = await rejectAvis(avis._id, reason);
         this.props.onChange(updated);
-        if (reason === 'injure') {
-            this.openModal();
-        }
     };
 
-    openModal = () => {
-        this.setState({ modalIsOpen: true });
+     handleCancel = () => {
+        this.setState({ showModal: false });
     };
 
-    closeModal = () => {
-        this.setState({ modalIsOpen: false });
+     getModal = () => {
+        return (
+            <Modal
+                title="Rejeter cet avis pour injure"
+                text={
+                    <span>
+                        Le rejet pour Injure entraîne l'envoi d'un mail automatique au stagiaire pour l'informer que le <b>commentaire ne sera pas publié</b>. Il est invité à <b>laisser un nouvel avis</b>. Confirmez-vous cette demande ?
+                    </span>
+                }
+                onConfirmed={() => this.reject(this.props.avis, 'injure')}
+                onClose={this.handleCancel} />
+        );
     };
 
     getExtraClasses = () => {
@@ -42,6 +50,7 @@ export default class RejectButton extends React.Component {
 
         return (
             <div className="RejectButton btn-group">
+                {this.state.showModal && this.getModal()}
                 <button
                     type="button"
                     className={`btn btn-sm dropdown-toggle ${this.getExtraClasses()}`}
@@ -50,14 +59,13 @@ export default class RejectButton extends React.Component {
                 </button>
                 <div className="dropdown-menu">
                     <h6 className="dropdown-header">Rejeter</h6>
-                    <a className="dropdown-item" onClick={() => this.reject(avis, 'injure')}>Injure</a>
+                    <a className="dropdown-item" onClick={() => this.setState({ showModal: true })}>Injure</a>
                     <div className="dropdown-divider" />
                     <a className="dropdown-item" onClick={() => this.reject(avis, 'alerte')}>Alerte</a>
                     <div className="dropdown-divider" />
                     <a className="dropdown-item" onClick={() => this.reject(avis, 'non concerné')}>
                         Non concerné
                     </a>
-                    <InjureTagModal modalIsOpen={this.state.modalIsOpen} closeModal={this.closeModal}/>
                 </div>
             </div>
         );
