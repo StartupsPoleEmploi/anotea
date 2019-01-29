5e4583ee69589e1c3766a1d094ac26d09711c8f3
Loïc Guillois <contact@loicguillois.fr>
2019-01-29 17:32:36 +0100

    logo + meta data

--- questionnaire/src/components/Header.jsx
+++ questionnaire/src/components/Header.jsx
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import './header.scss';
 
 class Header extends Component {
+
   render() {
     return (
       <div>
-          <h1>Notez et commentez votre formation</h1>
-          <h2><strong>Compétences de base Professionnelles</strong> | 09/07/2018 au 31/12/2018 
+        <img className="logo" src="/images/logo.png" alt="logo Anotéa" />
+        <h1>Notez et commentez votre formation</h1>
+        <h2><strong>Compétences de base Professionnelles</strong> | 09/07/2018 au 31/12/2018 
 GRETA DE L'ESSONNE - Épinay-sous-Sénart
-</h2>
+        </h2>
       </div>
     );
   }
