0a725d1fbde4f5439e81ca680cdfc8c47a67d719
unknown <nabil.tri@gmail.com>
2019-01-30 12:23:53 +0100

    autorisations component

--- questionnaire/src/App.js
+++ questionnaire/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Notes from './components/Notes';
 import Commentaire from './components/Commentaire';
 import Footer from './components/Footer';
+import Autorisations from './components/Autorisations';
 
 import './App.css';
 
@@ -12,10 +13,12 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        
+
         <Notes />
 
         <Commentaire />
+
+        <Autorisations />
 
         <Footer codeRegion="11" />
       </div>
