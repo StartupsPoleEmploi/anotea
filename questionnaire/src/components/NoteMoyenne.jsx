861a2d93fde5d8d1d19d22ae0d0a1476c8568a09
unknown <nabil.tri@gmail.com>
2019-01-31 14:58:06 +0100

    details of notes bloc

--- questionnaire/src/components/NoteMoyenne.jsx
+++ questionnaire/src/components/NoteMoyenne.jsx
@@ -8,11 +8,17 @@ class NoteMoyenne extends Component {
 
     render() {
     return (
-      <div className="note odd">
-        <span className="title">Note moyenne</span>
-        <span className="description">Voici la moyenne des notes que vous avez données.</span>
-        <span className="score">{this.props.averageScore}</span>
-        <Stars />
+      <div>
+        <div className="note odd">
+          <span className="title">Note moyenne</span>
+          <span className="description">Voici la moyenne des notes que vous avez données.</span>
+          <span className="score">{this.props.averageScore}</span>
+          <Stars />
+        </div>
+        <div className="note_details">
+          <span className="note_details_title">Détails des notes</span>
+          <span className="arrow"></span>
+        </div>
       </div>
     );
   }
