f79a9b90bb7622a88071a922f23ae2f5fd235bf0
Benoît Guérout <bguerout@yonoma.com>
2019-02-06 10:21:15 +0100

    Add carif into dataset regional data

--- backend/src/jobs/import/dataset/createRegionalData.js
+++ backend/src/jobs/import/dataset/createRegionalData.js
@@ -5,5 +5,13 @@ module.exports = async db => {
     return Promise.all([
         Promise.all(departements.map(departement => db.collection('departements').insertOne(departement))),
         Promise.all(regions.map(region => db.collection('regions').insertOne(region))),
+        db.collection('carif').insertOne({
+            codeRegion: '11',
+            name: 'Carif web',
+            courriel: 'anotea.pe@pole-emploi.fr',
+            url: 'https://anotea.pole-emploi.fr',
+            formLinkEnabled: true,
+            carifNameHidden: false
+        })
     ]);
 };
