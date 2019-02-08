17a832bb85bb2b30753479229f0bd33f576b396a
Benoît Guérout <bguerout@yonoma.com>
2019-02-08 16:56:25 +0100

    Sort dumped avis by date

--- backend/src/jobs/data/dataset/dumpAvis.js
+++ backend/src/jobs/data/dataset/dumpAvis.js
@@ -5,11 +5,11 @@ module.exports = async (db, dumpFile) => {
     let projection = { 'pseudo': 1, 'comment': 1, 'training.title': 1, 'training.organisation.name': 1, '_id': 0 };
     let [published, rejected, reported, toModerate] = await Promise.all([
 
-        db.collection('comment').find({ published: true }).project(projection).limit(5).toArray(),
+        db.collection('comment').find({ published: true }).project(projection).sort({ date: -1 }).limit(5).toArray(),
 
-        db.collection('comment').find({ rejected: true }).project(projection).limit(5).toArray(),
+        db.collection('comment').find({ rejected: true }).project(projection).sort({ date: -1 }).limit(5).toArray(),
 
-        db.collection('comment').find({ reported: true }).project(projection).limit(5).toArray(),
+        db.collection('comment').find({ reported: true }).project(projection).sort({ date: -1 }).limit(5).toArray(),
 
         db.collection('comment').find({ moderated: { $exists: false }, comment: { $exists: true } })
         .project(projection)
