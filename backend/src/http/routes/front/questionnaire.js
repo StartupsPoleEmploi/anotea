eb0fadc14e0bc8daedd070c31ae4d9f02328c6a2
Loïc Guillois <contact@loicguillois.fr>
2019-02-11 17:34:44 +0100

    new questionnaire : backend api

--- backend/src/http/routes/front/questionnaire.js
+++ backend/src/http/routes/front/questionnaire.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const moment = require('moment');
 const { getDeviceType } = require('./utils/analytics');
+const Joi = require('joi');
+const s = require('string');
+const externalLinks = require('./utils/externalLinks');
+const { sanitize } = require('./utils/userInput');
 
-module.exports = ({ db, logger }) => {
+module.exports = ({ db, logger, configuration }) => {
 
     const router = express.Router(); // eslint-disable-line new-cap
+    let badwords = require('./utils/badwords')(logger, configuration);
 
     const getTraineeFromToken = (req, res, next) => {
         db.collection('trainee').findOne({ token: req.params.token })
@@ -66,6 +71,106 @@ module.exports = ({ db, logger }) => {
             res.send({ error: true, reason: 'already sent', trainee: trainee });
         } else {
             res.send({ trainee: trainee });
+        }
+    });
+
+    router.post('/questionnaire/:token', getTraineeFromToken, async (req, res) => {
+
+        let trainee = req.trainee;
+        let comment = await db.collection('comment').findOne({
+            token: req.params.token,
+            formacode: trainee.training.formacode,
+            idSession: trainee.training.idSession
+        });
+
+        // we let user change it's advice if last step not validated
+        if (comment !== null && comment.step === 3) {
+            res.send({ error: true, reason: 'already sent', trainee: trainee });
+        } else if (comment !== null) {
+
+            const rateRule = Joi.number().integer().min(1).max(5).required();
+
+            const schema = Joi.object().keys({
+                accueil: rateRule,
+                contenu_formation: rateRule,
+                equipe_formateurs: rateRule,
+                moyen_materiel: rateRule,
+                accompagnement: rateRule,
+                global: rateRule,
+            });
+            let rates = {
+                accueil: req.body.avis_accueil,
+                contenu_formation: req.body.avis_contenu_formation,
+                equipe_formateurs: req.body.avis_equipe_formateurs,
+                moyen_materiel: req.body.avis_moyen_materiel,
+                accompagnement: req.body.avis_accompagnement,
+                global: req.body.avis_global
+            };
+
+            const result = Joi.validate(rates, schema);
+            if (result.error === null) {
+                comment.rates = result.value;
+                comment.accord = false;
+                comment.accordEntreprise = false;
+                comment.step = 3;
+
+                let pseudo = sanitize(req.body.pseudo);
+                let commentTxt = sanitize(req.body.commentaire);
+                let commentTitle = sanitize(req.body.titreCommentaire);
+
+                if (s(pseudo.replace(/ /g, '')).isAlphaNumeric()) {
+                    comment.pseudo = pseudo;
+                    if (commentTitle !== '' || commentTxt !== '') {
+                        comment.comment = {
+                            title: commentTitle,
+                            text: commentTxt
+                        };
+                    }
+                    comment.accord = req.body.accord === 'on';
+                    comment.accordEntreprise = req.body.accordEntreprise === 'on';
+
+                    let pseudoOK = badwords.isGood(pseudo);
+                    let commentOK = badwords.isGood(commentTxt);
+                    let commentTitleOK = badwords.isGood(commentTitle);
+                    if (pseudoOK && commentOK && commentTitleOK) {
+                        comment.step = 3;
+                        delete comment.badwords;
+                        db.collection('comment').updateOne({ _id: comment._id }, { $set: comment });
+                        res.redirect(`/questionnaire/${req.params.token}/step3`);
+                        return;
+                    } else {
+                        let badwords = {
+                            pseudo: !pseudoOK,
+                            comment: !commentOK,
+                            commentTitle: !commentTitleOK
+                        };
+                        res.send({ error: true, reason: 'badwords', badwords });
+                    }
+
+                    await Promise.all([
+                        db.collection('comment').updateOne({ _id: comment._id }, { $set: comment }),
+                        db.collection('trainee').updateOne({ _id: trainee._id }, { $set: { avisCreated: true } }),
+                    ]);
+
+                    const carif = await db.collection('carif').findOne({ codeRegion: trainee.codeRegion });
+
+                    const trainingTooOld = trainee.training.scheduledEndDate < moment().subtract(90, 'days');
+            
+                    res.send({
+                        trainee: trainee,
+                        carifURL: carif.url,
+                        carifLinkEnabled: carif.formLinkEnabled,
+                        showLinks: await externalLinks(db).getLink(trainee, 'pe') !== null && !trainingTooOld
+                    });
+                } else {
+                    res.send({ error: true, reason: 'alphanum', trainee });
+                    return;
+                }
+            } else {
+                res.send({ error: true, reason: 'bad data', trainee });
+            }
+        } else {
+            res.send({ error: true, reason: 'bad data', trainee });
         }
     });
 
