100dd9e34fa2e59679faf09549938cf3996520c3
Loïc Guillois <contact@loicguillois.fr>
2019-02-12 11:19:21 +0100

    questionnaire API

--- backend/src/http/routes/front/questionnaire.js
+++ backend/src/http/routes/front/questionnaire.js
@@ -41,10 +41,83 @@ module.exports = ({ db, logger, configuration }) => {
         next();
     };
 
+    const validateNotes = body => {
+        const rateRule = Joi.number().integer().min(1).max(5).required();
+
+        const schema = Joi.object().keys({
+            accueil: rateRule,
+            contenu_formation: rateRule,
+            equipe_formateurs: rateRule,
+            moyen_materiel: rateRule,
+            accompagnement: rateRule,
+            global: rateRule,
+        });
+        let rates = {
+            accueil: body.avis_accueil,
+            contenu_formation: body.avis_contenu_formation,
+            equipe_formateurs: body.avis_equipe_formateurs,
+            moyen_materiel: body.avis_moyen_materiel,
+            accompagnement: body.avis_accompagnement,
+            global: body.avis_global
+        };
+
+        return Joi.validate(rates, schema);
+    };
+
+    const validateAvis = (notes, body) => {
+        let avis = {};
+        avis.rates = notes;
+
+        let pseudo = sanitize(body.pseudo);
+        let commentTxt = sanitize(body.commentaire);
+        let commentTitle = sanitize(body.titreCommentaire);
+
+        if (s(pseudo.replace(/ /g, '')).isAlphaNumeric()) {
+            avis.pseudo = pseudo;
+            if (commentTitle !== '' || commentTxt !== '') {
+                avis.comment = {
+                    title: commentTitle,
+                    text: commentTxt
+                };
+            }
+            avis.accord = body.accord === 'on';
+            avis.accordEntreprise = body.accordEntreprise === 'on';
+
+            let pseudoOK = badwords.isGood(pseudo);
+            let commentOK = badwords.isGood(commentTxt);
+            let commentTitleOK = badwords.isGood(commentTitle);
+
+            if (pseudoOK && commentOK && commentTitleOK) {
+                return { error: null, avis };
+            } else {
+                let badwords = {
+                    pseudo: !pseudoOK,
+                    comment: !commentOK,
+                    commentTitle: !commentTitleOK
+                };
+                return { error: 'badwords', badwords };
+            }
+        } else {
+            return { error: 'alphanum' };
+        }
+    };
+
+    const getInfosRegion = async trainee => {
+        const carif = await db.collection('carif').findOne({ codeRegion: trainee.codeRegion });
+
+        const trainingTooOld = trainee.training.scheduledEndDate < moment().subtract(90, 'days');
+
+        return {
+            trainee: trainee,
+            carifURL: carif.url,
+            carifLinkEnabled: carif.formLinkEnabled,
+            showLinks: await externalLinks(db).getLink(trainee, 'pe') !== null && !trainingTooOld
+        };
+    };
+
     router.get('/questionnaire/:token/start', getTraineeFromToken, saveDeviceData, async (req, res) => {
 
         let trainee = req.trainee;
-        // We check if advice not already sent
         let comment = await db.collection('comment').findOne({
             token: req.params.token,
             formacode: trainee.training.formacode,
@@ -55,7 +128,7 @@ module.exports = ({ db, logger, configuration }) => {
             db.collection('trainee').updateOne({ token: req.params.token }, { $set: { 'tracking.click': new Date() } });
             res.send({ trainee: trainee });
         } else {
-            res.send({ error: true, reason: 'already sent', trainee: trainee });
+            res.send({ error: true, reason: 'already sent' });
         }
     });
 
@@ -68,94 +141,23 @@ module.exports = ({ db, logger, configuration }) => {
             idSession: trainee.training.idSession
         });
 
-        // we let user change it's advice if last step not validated
-        if (comment !== null && comment.step === 3) {
+        if (comment !== null) {
             res.send({ error: true, reason: 'already sent', trainee: trainee });
-        } else if (comment !== null) {
-
-            const rateRule = Joi.number().integer().min(1).max(5).required();
-
-            const schema = Joi.object().keys({
-                accueil: rateRule,
-                contenu_formation: rateRule,
-                equipe_formateurs: rateRule,
-                moyen_materiel: rateRule,
-                accompagnement: rateRule,
-                global: rateRule,
-            });
-            let rates = {
-                accueil: req.body.avis_accueil,
-                contenu_formation: req.body.avis_contenu_formation,
-                equipe_formateurs: req.body.avis_equipe_formateurs,
-                moyen_materiel: req.body.avis_moyen_materiel,
-                accompagnement: req.body.avis_accompagnement,
-                global: req.body.avis_global
-            };
-
-            const result = Joi.validate(rates, schema);
-            if (result.error === null) {
-                comment.rates = result.value;
-                comment.accord = false;
-                comment.accordEntreprise = false;
-                comment.step = 3;
-
-                let pseudo = sanitize(req.body.pseudo);
-                let commentTxt = sanitize(req.body.commentaire);
-                let commentTitle = sanitize(req.body.titreCommentaire);
-
-                if (s(pseudo.replace(/ /g, '')).isAlphaNumeric()) {
-                    comment.pseudo = pseudo;
-                    if (commentTitle !== '' || commentTxt !== '') {
-                        comment.comment = {
-                            title: commentTitle,
-                            text: commentTxt
-                        };
-                    }
-                    comment.accord = req.body.accord === 'on';
-                    comment.accordEntreprise = req.body.accordEntreprise === 'on';
-
-                    let pseudoOK = badwords.isGood(pseudo);
-                    let commentOK = badwords.isGood(commentTxt);
-                    let commentTitleOK = badwords.isGood(commentTitle);
-                    if (pseudoOK && commentOK && commentTitleOK) {
-                        comment.step = 3;
-                        delete comment.badwords;
-                        db.collection('comment').updateOne({ _id: comment._id }, { $set: comment });
-                        res.redirect(`/questionnaire/${req.params.token}/step3`);
-                        return;
-                    } else {
-                        let badwords = {
-                            pseudo: !pseudoOK,
-                            comment: !commentOK,
-                            commentTitle: !commentTitleOK
-                        };
-                        res.send({ error: true, reason: 'badwords', badwords });
-                    }
-
+        } else {
+            let resultNotes = validateNotes(req.body);
+            if (resultNotes.error === null) {
+                let resultAvis = validateAvis(resultNotes.value, req.body);
+                if (resultAvis.error === null) {
                     await Promise.all([
-                        db.collection('comment').updateOne({ _id: comment._id }, { $set: comment }),
+                        db.collection('comment').saveOne(resultAvis.avis),
                         db.collection('trainee').updateOne({ _id: trainee._id }, { $set: { avisCreated: true } }),
                     ]);
-
-                    const carif = await db.collection('carif').findOne({ codeRegion: trainee.codeRegion });
-
-                    const trainingTooOld = trainee.training.scheduledEndDate < moment().subtract(90, 'days');
-            
-                    res.send({
-                        trainee: trainee,
-                        carifURL: carif.url,
-                        carifLinkEnabled: carif.formLinkEnabled,
-                        showLinks: await externalLinks(db).getLink(trainee, 'pe') !== null && !trainingTooOld
-                    });
+                    let infos = await getInfosRegion(trainee);
+                    res.send(infos);
                 } else {
-                    res.send({ error: true, reason: 'alphanum', trainee });
-                    return;
+                    res.send({ error: true, reason: resultAvis.error });
                 }
-            } else {
-                res.send({ error: true, reason: 'bad data', trainee });
             }
-        } else {
-            res.send({ error: true, reason: 'bad data', trainee });
         }
     });
 
