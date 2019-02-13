661994d23511455082f48e8cfe039a9b5f2c4de1
Loïc Guillois <contact@loicguillois.fr>
2019-02-13 15:51:30 +0100

    show remaining chars

--- questionnaire/src/components/ChampCommentaire.jsx
+++ questionnaire/src/components/ChampCommentaire.jsx
@@ -3,20 +3,37 @@ import PropTypes from 'prop-types';
 
 import './ChampCommentaire.scss';
 
+const LIMIT = 200;
+
 class ChampCommentaire extends Component {
+
+    state = {
+        value: ''
+    }
 
     static propTypes = {
         titre: PropTypes.string.isRequired,
         placeholder: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
-        onChange: PropTypes.func.isRequired
+        onChange: PropTypes.func.isRequired,
     };
+
+    onChange = event => {
+        let value = event.target.value;
+        if (value.length <= LIMIT) {
+            this.setState({ value: value });
+            this.props.onChange(event);
+        }
+    }
 
     render() {
         return (
             <div className="champ-commentaire">
                 <span className={`title_${this.props.name}`}><strong>{this.props.titre}</strong> (optionnel)</span>
-                <textarea className={`textarea_${this.props.name}`} type="text" placeholder={this.props.placeholder} name={this.props.name} onChange={this.props.onChange} />
+                <textarea className={`textarea_${this.props.name}`} type="text" placeholder={this.props.placeholder} name={this.props.name} value={this.state.value} onChange={this.onChange} />
+                { this.state.value.length > 0 &&
+                    <div className={this.state.value.length === LIMIT ? 'remaining no' : 'remaining yes'}>{LIMIT - this.state.value.length} caractères restant</div>
+                }
             </div>
         );
     }
