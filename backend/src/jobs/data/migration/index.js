15d88d1abb0aeea8d9d4118e731137bd955bc630
Benoît Guérout <bguerout@yonoma.com>
2019-02-13 17:30:24 +0100

    Reset migrations scripts

--- backend/src/jobs/data/migration/index.js
+++ backend/src/jobs/data/migration/index.js
@@ -4,6 +4,5 @@
 const { execute } = require('../../job-utils');
 
 execute(async ({ db }) => {
-    await require('./merge-account')(db);
-    await require('./rename-password-to-passwordHash')(db);
+    await require('./remove-old-accounts')(db);
 });
