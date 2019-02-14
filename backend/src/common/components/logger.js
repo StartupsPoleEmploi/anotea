7e36be097f6b432afbfb8e8bda5fd6ec3b6cc501
Benoît Guérout <bguerout@yonoma.com>
2019-02-14 18:59:13 +0100

    Add stream to log to a file (with rotation)

--- backend/src/common/components/logger.js
+++ backend/src/common/components/logger.js
@@ -3,28 +3,38 @@ const bunyan = require('bunyan');
 
 module.exports = (name, configuration) => {
 
-    const prettyStream = () => {
+    const prettyConsoleStream = () => {
         let pretty = new PrettyStream();
         pretty.pipe(process.stdout);
         return {
-            name: 'pretty',
             level: configuration.log.level,
             stream: pretty,
         };
     };
 
-    const jsonStream = () => {
+    const jsonConsoleStream = () => {
         return {
-            name: 'json',
             level: configuration.log.level,
             stream: process.stdout,
         };
     };
 
+    const fileStream = () => {
+        return {
+            type: 'rotating-file',
+            level: configuration.log.level,
+            path: configuration.log.file,
+            period: '1d',
+            count: 31,
+        };
+    };
 
     return bunyan.createLogger({
         name,
         serializers: bunyan.stdSerializers,
-        streams: [configuration.log.json ? jsonStream() : prettyStream()],
+        streams: [
+            ...(configuration.log.console === 'json' ? [jsonConsoleStream()] : [prettyConsoleStream()]),
+            ...(configuration.log.file ? [fileStream()] : [])
+        ],
     });
 };
