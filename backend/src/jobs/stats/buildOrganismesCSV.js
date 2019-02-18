4ff2387c2f180be85e7de99354149f7b49f3f960
Benoît Guérout <bguerout@yonoma.com>
2019-02-18 09:38:26 +0100

    Ouverture Bourgogne-Franche-Comté et Normandie

--- backend/src/jobs/stats/buildOrganismesCSV.js
+++ backend/src/jobs/stats/buildOrganismesCSV.js
@@ -2,25 +2,33 @@
 'use strict';
 
 const fs = require('fs');
+const cli = require('commander');
 const { encodeStream } = require('iconv-lite');
 const path = require('path');
 const getOrganismeEmail = require('../../common/utils/getOrganismeEmail');
 const { execute } = require('../job-utils');
 
+cli.description('Export organismes per active region')
+.parse(process.argv);
+
 execute(async ({ logger, db, configuration }) => {
 
     logger.info('Building email statistics displayed on financer dashboard');
 
-    const generateCSV = codeRegion => {
+    const generateCSV = ({ name, code_region: codeRegion }) => {
         return new Promise((resolve, reject) => {
             let total = 0;
-            let output = fs.createWriteStream(path.join(__dirname, `../../../../.data/organismes-${codeRegion}.csv`));
+            let output = fs.createWriteStream(path.join(__dirname, `../../../../.data/organismes-${name}-${codeRegion}.csv`));
 
-            output.write(`Siret;Raison sociale;Email;Nombre Avis\n`);
+            output.write(`Siret;Raison sociale;Email;Nombre Avis;Kairos\n`);
 
             db.collection('accounts').find({ profile: 'organisme', codeRegion }).transformStream({
                 transform: organisme => {
-                    return `="${organisme.meta.siretAsString}";"${organisme.raisonSociale}";"${getOrganismeEmail(organisme)}";"${organisme.meta.nbAvis || 0}"\n`;
+                    let kairos = !!organisme.sources.find(s => s === 'kairos');
+                    let email = getOrganismeEmail(organisme);
+                    let nbAvis = organisme.score.nb_avis;
+
+                    return `="${organisme.meta.siretAsString}";"${organisme.raisonSociale}";"${email}";"${nbAvis}";"${kairos ? 'oui' : 'non'}"\n`;
                 }
             })
             .on('data', () => total++)
@@ -36,7 +44,7 @@ execute(async ({ logger, db, configuration }) => {
 
     logger.info(`Generating CSV file...`);
 
-    let activeRegions = configuration.app.active_regions.map(region => region.code_region);
-    await Promise.all(activeRegions.map(codeRegion => generateCSV(codeRegion)));
+    let activeRegions = configuration.app.active_regions;
+    await Promise.all(activeRegions.map(region => generateCSV(region)));
 
 });
