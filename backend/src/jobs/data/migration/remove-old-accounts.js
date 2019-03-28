d8444c058375bac966c925555f329fcd56a53611
Benoît Guérout <bguerout@yonoma.com>
2019-03-28 08:31:38 +0100

    Remove migration scripts

--- backend/src/jobs/data/migration/remove-old-accounts.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-module.exports = db => {
-    return Promise.all([
-        db.collection('moderator').drop(),
-        db.collection('financer').drop(),
-    ]);
-};
