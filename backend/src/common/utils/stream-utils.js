1fe43fa5271c851df55749fe6894cd162ddaf951
Benoît Guérout <bguerout@yonoma.com>
2019-04-15 15:37:00 +0200

    Add ignoreEmptry stream util

--- backend/src/common/utils/stream-utils.js
+++ backend/src/common/utils/stream-utils.js
@@ -1,29 +1,31 @@
 const _ = require('lodash');
 const { Transform } = require('stream');
 
-module.exports = {
-    transformObject: (callback, options = {
-        ignoreFirstLine: false,
-        ignoreEmpty: false,
-        readableObjectMode: true,
-        writableObjectMode: true
-    }) => {
-        let lines = 0;
-        return new Transform({
-            readableObjectMode: options.readableObjectMode !== undefined ? options.readableObjectMode : true,
-            writableObjectMode: options.writableObjectMode !== undefined ? options.writableObjectMode : true,
-            transform: async function(data, encoding, next) {
-                if ((options.ignoreEmpty && _.isEmpty(data)) ||
-                    (options.ignoreFirstLine && lines++ === 0)) {
-                    return next();
-                }
-
-                let res = await callback(data);
-                this.push(res);
-                next();
+let transformObject = (callback, options = {
+    ignoreFirstLine: false,
+    ignoreEmpty: false,
+    readableObjectMode: true,
+    writableObjectMode: true
+}) => {
+    let lines = 0;
+    return new Transform({
+        readableObjectMode: options.readableObjectMode !== undefined ? options.readableObjectMode : true,
+        writableObjectMode: options.writableObjectMode !== undefined ? options.writableObjectMode : true,
+        transform: async function(data, encoding, next) {
+            if ((options.ignoreEmpty && _.isEmpty(data)) ||
+                (options.ignoreFirstLine && lines++ === 0)) {
+                return next();
             }
-        });
-    },
+
+            let res = await callback(data);
+            this.push(res);
+            next();
+        }
+    });
+};
+module.exports = {
+    transformObject: transformObject,
+    ignoreEmpty: () => transformObject(data => data, { ignoreEmpty: true }),
     jsonStream: () => {
         let chunksSent = 0;
         return new Transform({
