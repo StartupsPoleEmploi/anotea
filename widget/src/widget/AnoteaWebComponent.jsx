ddb1e46953243f8d25cde23ea52d4b3ad8d23547
Loïc Guillois <contact@loicguillois.fr>
2019-05-06 11:36:47 +0200

    fix width

--- widget/src/widget/AnoteaWebComponent.jsx
+++ widget/src/widget/AnoteaWebComponent.jsx
@@ -10,9 +10,9 @@ class WidgetAnotea extends HTMLElement {
         const shadowRoot = widget.attachShadow({mode: 'closed'});
         const layout = this.getAttribute('layout');
         if(layout === 'tiny') {
-            ReactDOM.render(<AnoteaTinyWidget width={this.getAttribute('width')} niveau={this.getAttribute('niveau')} siret={this.getAttribute('siret')} numeroAction={this.getAttribute('numeroAction')} />, shadowRoot);
+            ReactDOM.render(<AnoteaTinyWidget niveau={this.getAttribute('niveau')} siret={this.getAttribute('siret')} numeroAction={this.getAttribute('numeroAction')} />, shadowRoot);
         } else {
-            ReactDOM.render(<AnoteaWidget layout={layout} niveau={this.getAttribute('niveau')} siret={this.getAttribute('siret')} numeroAction={this.getAttribute('numeroAction')} numeroSession={this.getAttribute('numeroSession')} numeroFormation={this.getAttribute('numeroFormation')} />, shadowRoot);
+            ReactDOM.render(<AnoteaWidget width={this.getAttribute('width')} layout={layout} niveau={this.getAttribute('niveau')} siret={this.getAttribute('siret')} numeroAction={this.getAttribute('numeroAction')} numeroSession={this.getAttribute('numeroSession')} numeroFormation={this.getAttribute('numeroFormation')} />, shadowRoot);
         }
     }
 }
