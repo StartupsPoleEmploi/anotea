fbf7b8cc8ea8ec83d211623df3d8256d1979e1cc
Benoît Guérout <bguerout@yonoma.com>
2019-05-22 15:46:33 +0200

    Fix widget pagination when requesting formation/action/session

--- widget/src/components/services/formationService.js
+++ widget/src/components/services/formationService.js
@@ -1,8 +1,35 @@
 import { _get } from '../../utils/http-client';
 
-export const getFormation = numero => _get(`/api/v1/formations/${numero}&notes_decimales=true`);
+let emulatePaginatedAvisList = doc => {
+    let avis = doc.avis.filter(avis => avis.commentaire);
+    return {
+        score: doc.score,
+        results: {
+            avis,
+            meta: {
+                pagination: {
+                    page: 0,
+                    items_par_page: avis.length,
+                    total_items: avis.length,
+                    total_pages: 1,
+                }
+            }
+        },
+    };
+};
 
-export const getAction = numero => _get(`/api/v1/actions/${numero}&notes_decimales=true`);
+export const getFormation = async id => {
+    let formation = await _get(`/api/v1/formations/${id}?notes_decimales=true`);
+    return emulatePaginatedAvisList(formation);
+};
 
-export const getSession = numero => _get(`/api/v1/sessions/${numero}&notes_decimales=true`);
+export const getAction = async id => {
+    let action = await _get(`/api/v1/actions/${id}?notes_decimales=true`);
+    return emulatePaginatedAvisList(action);
+};
+
+export const getSession = async id => {
+    let session = await _get(`/api/v1/sessions/${id}?notes_decimales=true`);
+    return emulatePaginatedAvisList(session);
+};
 
