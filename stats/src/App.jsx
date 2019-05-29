4bb225c488a330568f8647bd2400860f999c4cec
nabilou <nabil.tri@gmail.com>
2019-05-29 16:48:04 +0200

    install node-sass & add components

--- stats/src/App.jsx
+++ stats/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header'
+
 function App() {
-  return (
-    <div className="anotea">
-      
-    </div>
-  );
+    return (
+        <div className="anotea">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ stats/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+    return (
+        <div className="Header">
+            <div className="title">A header</div>
+        </div>
+    );
+}
