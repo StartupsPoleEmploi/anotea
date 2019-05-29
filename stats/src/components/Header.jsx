ca3854ff7c2e7114a491777b9259e65b0420fa37
nabilou <nabil.tri@gmail.com>
2019-05-29 17:26:38 +0200

    use grid property & add logo to the header

--- stats/src/components/Header.jsx
+++ stats/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.scss';
+import logo from './images/logo-anotea.png'
 
 export default function Header() {
     return (
         <div className="Header">
+            <img className="logo" src={logo} alt="logo Anotea" />
             <div className="title">A header</div>
         </div>
     );
