142b8880cf79b44d67d9a84fa59f3693d8a3ce34
Benoît Guérout <bguerout@yonoma.com>
2019-05-29 12:05:04 +0200

    Rename trainee folder into stagiaires

--- backend/test/integration/jobs/import/stagiaires/traineeImporter-test.js
+++ backend/test/integration/jobs/import/stagiaires/traineeImporter-test.js
@@ -5,8 +5,8 @@ const assert = require('assert');
 const md5File = require('md5-file/promise');
 const { withMongoDB } = require('../../../../helpers/test-database');
 const logger = require('../../../../helpers/test-logger');
-const traineeImporter = require('../../../../../src/jobs/import/trainee/traineeImporter');
-const poleEmploiCSVHandler = require('../../../../../src/jobs/import/trainee/handlers/poleEmploiCSVHandler');
+const traineeImporter = require('../../../../../src/jobs/import/stagiaires/traineeImporter');
+const poleEmploiCSVHandler = require('../../../../../src/jobs/import/stagiaires/handlers/poleEmploiCSVHandler');
 
 describe(__filename, withMongoDB(({ getTestDatabase, getComponents }) => {
 
