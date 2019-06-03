87c8aaa71f3a75f1a76bf4f50fdae76262c08afb
Loïc Guillois <contact@loicguillois.fr>
2019-06-03 14:20:22 +0200

    configure region Bretagne

--- backend/test/unit/regions-test.js
+++ backend/test/unit/regions-test.js
@@ -7,7 +7,7 @@ describe(__filename, () => {
 
         let actives = regions().findActiveRegions();
         assert.deepStrictEqual(actives.map(region => region.codeRegion),
-            ['2', '3', '6', '7', '8', '10', '11', '13', '17', '18']);
+            ['2', '3', '4', '6', '7', '8', '10', '11', '13', '17', '18']);
     });
 
     it('can find region by code', () => {
