b8d7c86745a5548de7ca36f09b58ca0e7fd996d3
Loïc Guillois <contact@loicguillois.fr>
2019-06-04 16:58:42 +0200

    add tooltip on widget

--- widget/src/components/common/Notes.jsx
+++ widget/src/components/common/Notes.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Stars from './Stars';
 import './Notes.scss';
+import help from './images/help.png';
 
 export default class Notes extends Component {
+
+    state = {
+        tootlipAccueil: false,
+        tooltipAccompagnement: false
+    }
 
     static propTypes = {
         notes: PropTypes.object.isRequired,
     };
+
+    toggleTooltip = tooltip => this.setState(Object.assign(this.state, tooltip));
 
     render() {
 
@@ -16,11 +24,21 @@ export default class Notes extends Component {
         return (
             <div className="Notes d-flex flex-column">
                 <div className="d-flex">
-                    <div className="name">Accueil</div>
+                    <div className="name">Accueil <img src={help} className="help" onMouseEnter={this.toggleTooltip.bind(this, { tootlipAccueil: true })} onMouseLeave={this.toggleTooltip.bind(this, { tootlipAccueil: false })} /></div>
                     <div className="star ml-auto align-self-center">
                         <Stars note={notes.accueil} />
                     </div>
                 </div>
+
+                { this.state.tootlipAccueil &&
+                    <div>
+                        <div className="triangle"></div>
+                        <div className="widget-tooltip">
+                            Réunions d'information collective et entretiens à l'entrée en formation.
+                        </div>
+                    </div>
+                }
+
                 <div className="d-flex">
                     <div className="name">Contenu</div>
                     <div className="star ml-auto align-self-center">
@@ -40,11 +58,20 @@ export default class Notes extends Component {
                     </div>
                 </div>
                 <div className="d-flex">
-                    <div className="name">Accompagnement</div>
+                    <div className="name">Accompagnement <img src={help} className="help" onMouseEnter={this.toggleTooltip.bind(this, { tooltipAccompagnement: true })} onMouseLeave={this.toggleTooltip.bind(this, { tooltipAccompagnement: false })} /></div>
                     <div className="star ml-auto align-self-center">
                         <Stars note={notes.accompagnement} />
                     </div>
                 </div>
+
+                { this.state.tooltipAccompagnement &&
+                    <div>
+                        <div className="triangle accompagnement"></div>
+                        <div className="widget-tooltip">
+                            Aide à la recherche de stage/emploi, mise en relation et rencontre avec les entreprises.
+                        </div>
+                    </div>
+                }
             </div>
         );
     }
