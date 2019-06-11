b740825bd071c8974430d617ef985fd2cb6bdb7c
Benoît Guérout <bguerout@yonoma.com>
2019-06-11 08:32:16 +0200

    Remove previous migration script

--- backend/src/jobs/data/migration/index.js
+++ backend/src/jobs/data/migration/index.js
@@ -2,9 +2,7 @@
 'use strict';
 const { execute } = require('../../job-utils');
 
-const removeCarifCollection = require('./removeCarifCollection');
 
 execute(async ({ db }) => {
-    await removeCarifCollection(db);
 });
 
--- backend/src/jobs/data/migration/removeCarifCollection.js
+++
@@ -1,3 +0,0 @@
-module.exports = db => {
-    return db.collection('carif').drop();
-};
