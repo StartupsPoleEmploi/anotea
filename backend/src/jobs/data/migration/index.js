8f14209c036c9745cde72cd04622601e110d5f9a
Benoît Guérout <bguerout@yonoma.com>
2019-06-12 11:43:47 +0200

    Add script to remove invalid certif infos values

--- backend/src/jobs/data/migration/index.js
+++ backend/src/jobs/data/migration/index.js
@@ -2,7 +2,9 @@
 'use strict';
 const { execute } = require('../../job-utils');
 
+const cleanCertifInfos = require('./cleanCertifInfos');
 
 execute(async ({ db }) => {
+    await cleanCertifInfos(db);
 });
 
