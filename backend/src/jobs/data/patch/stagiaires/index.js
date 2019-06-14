2265871389ffdafb9cc14eb7c29c695dac9208b2
Benoît Guérout <bguerout@yonoma.com>
2019-06-14 13:59:24 +0200

    Add slack notifications when patching stagiaires

--- backend/src/jobs/data/patch/stagiaires/index.js
+++ backend/src/jobs/data/patch/stagiaires/index.js
@@ -7,9 +7,10 @@ const patchCertifinfos = require('./tasks/patchCertifInfos');
 const patchOrganismeResponsable = require('./tasks/patchOrganismeResponsable');
 
 cli.option('--certifInfos [certifInfos]', 'The CSV file with new certifInfos')
+.option('--slack', 'Send a slack notification when job is finished')
 .parse(process.argv);
 
-execute(async ({ logger, db, exit }) => {
+execute(async ({ logger, db, exit, sendSlackNotification }) => {
 
     if (!cli.certifInfos) {
         return exit('certifInfos file is required');
@@ -21,5 +22,10 @@ execute(async ({ logger, db, exit }) => {
         patchOrganismeResponsable(db, logger),
     ]);
 
+    sendSlackNotification({
+        text: `[STAGIAIRE] ${certifInfos.updated + organismeResponsable.updated} stagiaires mis à jour dont ` +
+            `${certifInfos.updated} certiInfos et ${organismeResponsable.updated} sirets`,
+    });
+
     return { certifInfos, organismeResponsable };
-});
+}, { slack: cli.slack });
