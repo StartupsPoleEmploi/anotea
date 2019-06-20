20f0983a95f5d42f477fe98bb2973bd4be2ecb2f
Benoît Guérout <bguerout@yonoma.com>
2019-06-20 08:38:21 +0200

    Convert stats tables into bootstrap

--- stats/src/App.jsx
+++ stats/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Header from './components/stats/Header';
-import AvisStats from './components/stats/AvisStats';
-import OrganismesStats from './components/stats/OrganismesStats';
-import './App.css';
+import AvisStatsTable from './components/stats/AvisStatsTable';
+import OrganismesStatsTable from './components/stats/OrganismesStatsTable';
+import GridDisplayer from './components/common/GridDisplayer';
+import './App.scss';
 
 class App extends Component {
 
@@ -16,14 +17,17 @@ class App extends Component {
                         <Redirect exact from="/" to="/stats/avis" />
                         <Redirect exact from="/stats" to="/stats/avis" />
                     </Switch>
-                    <Route
-                        path="/stats/avis"
-                        render={() => <AvisStats />}
-                    />
-                    <Route
-                        path="/stats/organismes"
-                        render={() => <OrganismesStats />}
-                    />
+                    <div className="container">
+                        <Route
+                            path="/stats/avis"
+                            render={() => <AvisStatsTable />}
+                        />
+                        <Route
+                            path="/stats/organismes"
+                            render={() => <OrganismesStatsTable />}
+                        />
+                    </div>
+                    {false && <GridDisplayer />}
                 </div>
             </Router>
         );
