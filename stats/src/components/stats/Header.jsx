1bc11bc35936fbcf67139db6865fa21a3900b714
Benoît Guérout <bguerout@yonoma.com>
2019-06-20 15:51:08 +0200

    Fix Header styles

--- stats/src/components/stats/Header.jsx
+++ stats/src/components/stats/Header.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
-import logo from '../../images/logo-anotea.png'
+import logo from '../../images/logo-anotea.png';
 
 export default class Header extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.view = this.props.view;
-    }
 
     render() {
 
         return (
             <div className="Header">
-                <img className="logo" src={logo} alt="logo Anotea" />
-                <div className="title d-flex justify-content-around">
-                    <NavLink to="/stats/avis" className="avis-title" activeClassName="active">
-                        Avis
-                    </NavLink>
-                    <NavLink to="/stats/organismes" className="organismes-title" activeClassName="active">
-                        Organismes
-                    </NavLink>
+                <div className="container">
+                    <div className="row d-flex align-items-center">
+                        <div className="col-sm-2">
+                            <img className="logo" src={logo} alt="logo Anotea" />
+                        </div>
+                        <div className="col-sm-10">
+                            <div className="d-flex justify-content-around">
+                                <NavLink to="/stats/avis" className="nav-link" activeClassName="active">
+                                    Avis
+                                </NavLink>
+                                <NavLink to="/stats/organismes" className="nav-link" activeClassName="active">
+                                    Organismes
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
