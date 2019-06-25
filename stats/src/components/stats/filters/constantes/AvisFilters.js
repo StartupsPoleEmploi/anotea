71821e93f7ba4f4ab4020076fe6b662a2d78c629
nabilou <nabil.tri@gmail.com>
2019-06-25 10:10:59 +0200

    must use singlequote

--- stats/src/components/stats/filters/constantes/AvisFilters.js
+++ stats/src/components/stats/filters/constantes/AvisFilters.js
@@ -1,97 +1,97 @@
 const AVIS_FILTERS = [
     {
-        intitule: "Campagne",
+        intitule: 'Campagne',
         items: [
-            { _id: 1, intitule: "Cumul depuis le début" },
+            { _id: 1, intitule: 'Cumul depuis le début' },
         ],
     },
     {
-        intitule: "Périmètre",
+        intitule: 'Périmètre',
         items: [
             {
-                codeINSEE: "2",
-                codeRegion: "13",
-                intitule: "Martinique"
+                codeINSEE: '2',
+                codeRegion: '13',
+                intitule: 'Martinique'
             },
             {
-                codeINSEE: "3",
-                codeRegion: "9",
-                intitule: "Guyane"
+                codeINSEE: '3',
+                codeRegion: '9',
+                intitule: 'Guyane'
             },
             {
-                codeINSEE: "4",
-                codeRegion: "12",
-                intitule: "La Réunion"
+                codeINSEE: '4',
+                codeRegion: '12',
+                intitule: 'La Réunion'
             },
             {
-                codeINSEE: "6",
-                codeRegion: "12",
-                intitule: "Mayotte"
+                codeINSEE: '6',
+                codeRegion: '12',
+                intitule: 'Mayotte'
             },
             {
-                codeINSEE: "11",
-                codeRegion: "11",
-                intitule: "IDF"
+                codeINSEE: '11',
+                codeRegion: '11',
+                intitule: 'IDF'
             },
             {
-                codeINSEE: "24",
-                codeRegion: "5",
-                intitule: "Centre-Val de Loire"
+                codeINSEE: '24',
+                codeRegion: '5',
+                intitule: 'Centre-Val de Loire'
             },
             {
-                codeINSEE: "27",
-                codeRegion: "3",
-                intitule: "Bourgogne-Franche-Comté"
+                codeINSEE: '27',
+                codeRegion: '3',
+                intitule: 'Bourgogne-Franche-Comté'
             },
             {
-                codeINSEE: "28",
-                codeRegion: "14",
-                intitule: "Normandie"
+                codeINSEE: '28',
+                codeRegion: '14',
+                intitule: 'Normandie'
             },
             {
-                codeINSEE: "32",
-                codeRegion: "10",
-                intitule: "Hauts-de-France"
+                codeINSEE: '32',
+                codeRegion: '10',
+                intitule: 'Hauts-de-France'
             },
             {
-                codeINSEE: "52",
-                codeRegion: "17",
-                intitule: "Pays de la Loire"
+                codeINSEE: '52',
+                codeRegion: '17',
+                intitule: 'Pays de la Loire'
             },
             {
-                codeINSEE: "53",
-                codeRegion: "4",
-                intitule: "Bretagne"
+                codeINSEE: '53',
+                codeRegion: '4',
+                intitule: 'Bretagne'
             },
             {
-                codeINSEE: "75",
-                codeRegion: "15",
-                intitule: "Nouvelle-Aquitaine"
+                codeINSEE: '75',
+                codeRegion: '15',
+                intitule: 'Nouvelle-Aquitaine'
             },
             {
-                codeINSEE: "84",
-                codeRegion: "2",
-                intitule: "ARA"
+                codeINSEE: '84',
+                codeRegion: '2',
+                intitule: 'ARA'
             },
             {
-                codeINSEE: "93",
-                codeRegion: "18",
-                intitule: "PACA"
+                codeINSEE: '93',
+                codeRegion: '18',
+                intitule: 'PACA'
             },
             {
-                codeINSEE: "94",
-                codeRegion: "6",
-                intitule: "Corse"
+                codeINSEE: '94',
+                codeRegion: '6',
+                intitule: 'Corse'
             },
             {
-                codeINSEE: "76",
-                codeRegion: "16",
-                intitule: "Occitanie"
+                codeINSEE: '76',
+                codeRegion: '16',
+                intitule: 'Occitanie'
             },
             {
-                codeINSEE: "44",
-                codeRegion: "7",
-                intitule: "Grand-Est"
+                codeINSEE: '44',
+                codeRegion: '7',
+                intitule: 'Grand-Est'
             }
         ],
     },
