520ac6dfaad7bddee10829326fd80eef7138115d
nabilou <nabil.tri@gmail.com>
2019-06-27 11:54:44 +0200

    add hover messages for OF part & change some wording

--- stats/src/components/stats/OrganismesStatsTable.jsx
+++ stats/src/components/stats/OrganismesStatsTable.jsx
@@ -48,13 +48,12 @@ export default class OrganismesStatsTable extends Component {
                         </th>
                         <th scope="col" className="section">Total</th>
                         <th scope="col" className="section">Total</th>
-                        <th scope="col">Ouverts <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col">Cliqués <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col" className="section">Actifs <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col" className="section">Non lus <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col" >Réponses <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col">Réponses avec commentaires <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
-                        <th scope="col">Signalés <i className="fas fa-question-circle"><span className="tooltip">Hover wording goes here</span></i></th>
+                        <th scope="col">Ouverts <i className="fas fa-question-circle"><span className="tooltip">Taux d&apos;ouverture de mail</span></i></th>
+                        <th scope="col">Cliqués <i className="fas fa-question-circle"><span className="tooltip">Taux de clic dans le lien</span></i></th>
+                        <th scope="col" className="section">Actifs <i className="fas fa-question-circle"><span className="tooltip">Taux des organismes actifs</span></i></th>
+                        <th scope="col" className="section">Non lus <i className="fas fa-question-circle"><span className="tooltip">Taux des avis non lus par les OF</span></i></th>
+                        <th scope="col">Répondus <i className="fas fa-question-circle"><span className="tooltip">Total des avis avec réponse</span></i></th>
+                        <th scope="col">Signalés <i className="fas fa-question-circle"><span className="tooltip">Taux des avis signalés</span></i></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -64,24 +63,20 @@ export default class OrganismesStatsTable extends Component {
                                 <th scope="row">{o.label}</th>
                                 <td className="section">{o.nbOrganismesContactes}</td>
                                 <td className="section">{o.mailsEnvoyes}</td>
-                                <td>{
-                                    this.computeRate(o.ouvertureMails, o.nbOrganismesContactes)}
+                                <td>
+                                    {this.computeRate(o.ouvertureMails, o.nbOrganismesContactes)}
                                 </td>
                                 <td>
                                     {this.computeRate(o.nbClicDansLien, o.ouvertureMails)}
                                 </td>
                                 <td className="section">
-                                    {
-                                        this.computeRate(o.organismesActifs, o.nbOrganismesContactes)}
+                                    {this.computeRate(o.organismesActifs, o.nbOrganismesContactes)}
                                 </td>
-                                <td className="section">{
-                                    this.computeRate(o.avisNonLus, o.avisModeresNonRejetes)}
-                                </td>
-                                <td>
-                                    {this.computeRate(o.nbReponsesAvecCommentaires, o.avisModeresNonRejetes)}
+                                <td className="section">
+                                    {this.computeRate(o.avisNonLus, o.avisModeresNonRejetes)}
                                 </td>
                                 <td>
-                                    {this.computeRate(o.nbReponses, o.avisModeresNonRejetes)}
+                                    {o.nbReponses + o.avisModeresNonRejetes}
                                 </td>
                                 <td>
                                     {this.computeRate(o.avisSignales, o.avisModeresNonRejetes)}
