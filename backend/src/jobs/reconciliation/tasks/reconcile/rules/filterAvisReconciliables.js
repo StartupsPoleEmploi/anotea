f06052b2c610480eb25803d7109c65f2024cb6ce
Benoît Guérout <bguerout@yonoma.com>
2019-07-02 16:16:51 +0200

    Limit avis reconciliables with code postal filter

--- backend/src/jobs/reconciliation/tasks/reconcile/rules/filterAvisReconciliables.js
+++ backend/src/jobs/reconciliation/tasks/reconcile/rules/filterAvisReconciliables.js
@@ -1,12 +1,10 @@
 const _ = require('lodash');
+const asSiren = require('./asSiren');
 
 module.exports = (action, comments) => {
     let all = comments.filter(c => {
-
-        let siren = action.organisme_formateur.siret_formateur.siret.substring(0, 9);
-
         return c.training.place.postalCode === action.lieu_de_formation.coordonnees.adresse.codepostal &&
-            new RegExp(`^${siren}`).test(c.training.organisation.siret);
+            asSiren(c.training.organisation.siret) === asSiren(action.organisme_formateur.siret_formateur.siret);
     });
 
     let certifiants = all.filter(c => !_.isEmpty(c.training.certifInfo.id));
