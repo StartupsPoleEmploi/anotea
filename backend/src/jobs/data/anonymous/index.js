b5c187ae4b907cb0f6d39ba0df37483bacd2ccd0
Loïc Guillois <contact@loicguillois.fr>
2019-07-04 16:12:52 +0200

    more accurate firstname/name/id finding + unit test + job that apply modifications to database

--- backend/src/jobs/data/anonymous/index.js
+++ backend/src/jobs/data/anonymous/index.js
@@ -4,11 +4,23 @@
 const cli = require('commander');
 const { execute } = require('../../job-utils');
 const exportCSV = require('./tasks/export');
+const doAnonymization = require('./tasks/doAnonymization');
 
 cli.description('Anonymize training title')
+.option('--export', 'Export result')
+.option('--apply', 'Apply anonymization to database')
 .parse(process.argv);
 
-execute(async ({ db, logger }) => {
-    logger.info('Anonymize training title...');
-    return exportCSV(db);
+execute(async ({ db, logger, exit }) => {
+
+    if (cli.export) {
+        logger.info('Export anonymized training title...');
+        return exportCSV(db);
+    } else if (cli.apply) {
+        logger.info('Anonymize training title...');
+        return doAnonymization(db);
+    } else {
+        return exit('Invalid arguments');
+    }
+
 });
---
+++ backend/src/jobs/data/anonymous/tasks/doAnonymization.js
@@ -0,0 +1,34 @@
+const { getAnonymizedTitle } = require('../utils')();
+
+module.exports = db => {
+    return new Promise((resolve, reject) => {
+        let stream = db.collection('trainee').find().sort().stream();
+    
+        stream.on('data', trainee => {
+            const result = getAnonymizedTitle(trainee);
+
+            if (result.changeDetected) {
+                const query = { token: trainee.token };
+
+                const update = {
+                    $set: {
+                        'training.title': result.anonymizedTitle,
+                        'anonymized': true,
+                        'training.oldTitle': trainee.training.title
+                    }
+                };
+
+                db.collection('trainee').updateOne(query, update);
+                db.collection('comment').updateOne({ token: trainee.token }, update);
+            }
+        });
+
+        stream.on('error', () => {
+            reject();
+        });
+
+        stream.on('end', () => {
+            resolve();
+        });
+    });
+};
