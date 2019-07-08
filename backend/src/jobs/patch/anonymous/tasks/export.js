c549fa9a3b8d3b85953841ca9c8dc90f3a30a7a1
Loïc Guillois <contact@loicguillois.fr>
2019-07-08 14:13:13 +0200

    fix id regex + more unit test + fix export

--- backend/src/jobs/patch/anonymous/tasks/export.js
+++ backend/src/jobs/patch/anonymous/tasks/export.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 const { getAnonymizedTitle } = require('../utils')();
+const getRegions = require('../../../../common/components/regions');
 
 module.exports = (db, path) => {
     return new Promise((resolve, reject) => {
         let streamOut = fs.createWriteStream(path);
+        let regions = getRegions();
 
         streamOut.once('open', function() {
-            streamOut.write('Prénom;Nom;Titre datalake;Titre anonymisé\n');
+            streamOut.write('Email stagiaire;Prénom;Nom;Titre datalake;Titre anonymisé;Date d\'import;Région;Avis déposés\n');
             let stream = db.collection('trainee').find().sort({ importDate: -1 }).stream();
         
             stream.on('data', trainee => {
                 const result = getAnonymizedTitle(trainee);
 
                 if (result.changeDetected) {
-                    streamOut.write(`${trainee.trainee.firstName};${trainee.trainee.name};${trainee.training.title};${result.anonymizedTitle};${trainee.importDate};${trainee.codeRegion}\n`);
+                    streamOut.write(`${trainee.trainee.email};${trainee.trainee.firstName};${trainee.trainee.name};${trainee.training.title};${result.anonymizedTitle};${trainee.importDate};${regions.findRegionByCodeRegion(trainee.codeRegion).nom};${trainee.avisCreated ? 'oui' : 'non'}\n`);
                 }
             });
 
