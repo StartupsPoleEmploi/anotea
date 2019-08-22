ee4dbb5f369f186ff3df17deb380b09a108226a6
nabilou <nabil.tri@gmail.com>
2019-08-22 10:38:18 +0200

    delete unused stats

--- backoffice/src/components/backoffice/financeur/FinancerPanel.jsx
+++ backoffice/src/components/backoffice/financeur/FinancerPanel.jsx
@@ -442,7 +442,7 @@ export default class FinancerPanel extends React.Component {
 
     render() {
         const { currentOrganisation, organisations, entities, formations, currentFormation } = this.state.training;
-        const { currentFinancer, financers, inventory, departements, currentLieu, startDate, endDate } = this.state;
+        const { currentFinancer, financers, inventory, departements, currentLieu } = this.state;
         const organisationsOptions = organisations.map(organisation => ({
             label: organisation.name.length >= 50 ? organisation.name.substring(0, 50).concat('...') : organisation.name,
             id: organisation._id,
