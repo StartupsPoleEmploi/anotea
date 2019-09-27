a63332395bd03355f8ef84431c5ba816f53ba990
Loïc Guillois <contact@loicguillois.fr>
2019-09-27 13:17:30 +0200

    recette env

--- backend/src/common/components/peconnect.js
+++ backend/src/common/components/peconnect.js
@@ -1,15 +1,11 @@
 const uuid = require('uuid');
 const request = require('request');
 
-const API_LOGIN_ROOT = 'https://authentification-candidat.pole-emploi.fr';
-
-const API_ROOT = 'https://api.emploi-store.fr/partenaire';
-
 const getState = () => uuid.v4().toString('base64');
 
 const getNonce = () => uuid.v4().toString('base64');
 
-const buildConnectionLink = (configuration, state, nonce) => `${API_LOGIN_ROOT}/connexion/oauth2/authorize?realm=%2Findividu&response_type=code&client_id=${configuration.peconnect.client_id}&scope=application_${configuration.peconnect.client_id}%20api_peconnect-individuv1%20openid%20profile%20email&redirect_uri=${configuration.peconnect.callback_url}&state=${state}&nonce=${nonce}`;
+const buildConnectionLink = (configuration, state, nonce) => `${configuration.peconnect.auth_base_url}/connexion/oauth2/authorize?realm=%2Findividu&response_type=code&client_id=${configuration.peconnect.client_id}&scope=application_${configuration.peconnect.client_id}%20api_peconnect-individuv1%20openid%20profile%20email&redirect_uri=${configuration.peconnect.callback_url}&state=${state}&nonce=${nonce}`;
 
 const checkState = (realState, state) => realState === state;
 
@@ -25,7 +21,7 @@ module.exports = configuration => {
                 link: buildConnectionLink(configuration, state, nonce)
             };
         },
-        checkState: (realState, state) => realState === state,
+        checkState: checkState,
         buildAccessToken: (configuration, code, nonce) => {
             return new Promise((resolve, reject) => {
                 const data = {
@@ -33,9 +29,9 @@ module.exports = configuration => {
                     code,
                     client_id: configuration.peconnect.client_id,
                     client_secret: configuration.peconnect.client_secret,
-                    redirect_uri: CALLBACK_URL
+                    redirect_uri: configuration.peconnect.callback_url
                 };
-                request.post({ url: `${API_LOGIN_ROOT}/connexion/oauth2/access_token?realm=%2Findividu`, form: data }, (error, response, body) => {
+                request.post({ url: `${configuration.peconnect.auth_base_url}/connexion/oauth2/access_token?realm=%2Findividu`, form: data }, (error, response, body) => {
                     let json = JSON.parse(body);
                     if (json.error) {
                         reject({ error: json.error });
@@ -50,7 +46,7 @@ module.exports = configuration => {
         getUserInfo: accessToken => {
             return new Promise((resolve, reject) => {
                 const auth = `Bearer ${accessToken}`;
-                request.get({ url: `${API_ROOT}/peconnect-individu/v1/userinfo`, headers: { Authorization: auth } }, (error, response, body) => {
+                request.get({ url: `${configuration.peconnect.api_base_url}/peconnect-individu/v1/userinfo`, headers: { Authorization: auth } }, (error, response, body) => {
                     let json = JSON.parse(body);
                     if (json.error) {
                         reject({ error: json.error });
@@ -60,5 +56,5 @@ module.exports = configuration => {
                 });
             });
         }
-    }
+    };
 };
