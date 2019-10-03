5f06d47b8d92a1c46112b4ae3a19b852f84c27c5
Benoît Guérout <bguerout@yonoma.com>
2019-10-03 14:01:37 +0200

    Export avis with encoding: 'UTF-16BE'

--- backend/src/http/routes/api/backoffice/avis-routes.js
+++ backend/src/http/routes/api/backoffice/avis-routes.js
@@ -69,7 +69,7 @@ module.exports = ({ db, middlewares, configuration, logger, moderation, consulta
         .stream();
 
         try {
-            await sendCSVStream(stream, res, avisCSVColumnsMapper(), { filename: 'avis.csv' });
+            await sendCSVStream(stream, res, avisCSVColumnsMapper(), { encoding: 'UTF-16BE', filename: 'avis.csv' });
         } catch (e) {
             //FIXME we must handle errors
             logger.error('Unable to send CSV file', e);
