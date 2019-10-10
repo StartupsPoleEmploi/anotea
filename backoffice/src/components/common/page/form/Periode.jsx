f21e1280bf4ba4a180c80ee19d3daeeeaae0d52d
Benoît Guérout <bguerout@yonoma.com>
2019-10-10 09:23:45 +0200

    Remove Periode icon (new UI)

--- backoffice/src/components/common/page/form/Periode.jsx
+++ backoffice/src/components/common/page/form/Periode.jsx
@@ -24,8 +24,7 @@ export default class Periode extends React.Component {
         let { periode, min } = this.props;
 
         return (
-            <div className="Periode d-flex justify-content-between align-items-center">
-                <i className="ml-2 far fa-calendar"></i>
+            <div className="Periode d-flex justify-content-around align-items-center">
                 <Date
                     value={periode.startDate}
                     min={min}
@@ -37,7 +36,7 @@ export default class Periode extends React.Component {
                     min={periode.startDate || min}
                     onChange={date => this.onDateChange('endDate', date)}
                 />
-                <span className="clear mr-2" onClick={() => this.onReset()}>
+                <span className="clear" onClick={() => this.onReset()}>
                     <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-tj5bde-Svg"><path
                         d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
                 </span>
