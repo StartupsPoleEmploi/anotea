d0b6715e0411885df4404b322f14d879c6043df3
Benoît Guérout <bguerout@yonoma.com>
2019-10-17 11:20:04 +0200

    Polish reponse status rendering

--- backoffice/src/components/common/avis/Reponse.jsx
+++ backoffice/src/components/common/avis/Reponse.jsx
@@ -42,11 +42,13 @@ export default class Reponse extends React.Component {
 
         return (
             <div className="Reponse">
-                <div className="title">
-                    <span>Réponse de l&apos;organisme </span>
-                    <Status avis={avis} />
+                <div className="d-flex justify-content-between align-items-baseline">
+                    <div>
+                        <span className="title">Réponse de l&apos;organisme </span>
+                        <Status avis={avis} />
+                    </div>
                     {avis.reponse.date &&
-                    <span className="date float-right">le <PrettyDate date={new Date(avis.reponse.date)} /></span>
+                    <span className="date">le <PrettyDate date={new Date(avis.reponse.date)} /></span>
                     }
                 </div>
                 <p>{avis.reponse.text}</p>
