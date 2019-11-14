543961f2b148c021d7423af8fe79516883a383c6
Benoît Guérout <bguerout@yonoma.com>
2019-11-14 19:38:20 +0100

    Print urls when dataset has been generated

--- backend/src/jobs/stats/index.js
+++ backend/src/jobs/stats/index.js
@@ -3,31 +3,10 @@
 
 const cli = require('commander');
 const { execute } = require('../job-utils');
-const computeOrganismesStats = require('./tasks/computeOrganismesStats');
-const computeAvisStats = require('./tasks/computeAvisStats');
-const computeApiStats = require('./tasks/computeApiStats');
-const computeCampaignStats = require('./tasks/computeCampaignStats');
+const computeStats = require('./tasks/computeStats');
 
 cli.parse(process.argv);
 
 execute(async ({ db, regions }) => {
-
-    let stats = await Promise.all([
-        computeOrganismesStats(db, regions),
-        computeAvisStats(db, regions),
-        computeApiStats(db, regions),
-        computeCampaignStats(db, regions),
-    ]);
-
-    let doc = {
-        date: new Date(),
-        organismes: stats[0],
-        avis: stats[1],
-        api: stats[2],
-        campaign: stats[3],
-    };
-
-    await db.collection('statistics').insertOne(doc);
-
-    return doc;
+    return computeStats(db, regions);
 });
---
+++ backend/src/jobs/stats/tasks/computeStats.js
@@ -0,0 +1,22 @@
+const computeOrganismesStats = require('./stats/computeOrganismesStats');
+const computeAvisStats = require('./stats/computeAvisStats');
+const computeApiStats = require('./stats/computeApiStats');
+const computeCampaignStats = require('./stats/computeCampaignStats');
+
+module.exports = async (db, regions) => {
+
+    let stats = await Promise.all([
+        computeOrganismesStats(db, regions),
+        computeAvisStats(db, regions),
+        computeApiStats(db, regions),
+        computeCampaignStats(db, regions),
+    ]);
+
+    return db.collection('statistics').insertOne({
+        date: new Date(),
+        organismes: stats[0],
+        avis: stats[1],
+        api: stats[2],
+        campaign: stats[3],
+    });
+};
