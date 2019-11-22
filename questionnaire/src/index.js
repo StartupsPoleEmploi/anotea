1f70ab39cc4fb3ca4b51bf8da285d36798d1189f
Benoît Guérout <bguerout@yonoma.com>
2019-11-22 14:31:51 +0100

    Use react-app-polyfill instead of polyfill.io

--- questionnaire/src/index.js
+++ questionnaire/src/index.js
@@ -1,4 +1,5 @@
-import 'whatwg-fetch';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'jquery/dist/jquery.min.js';
