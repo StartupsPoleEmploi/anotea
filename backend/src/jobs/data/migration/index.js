c985c340b71357647cb95d36c8a7af932f535d3a
Benoît Guérout <bguerout@yonoma.com>
2019-11-27 08:52:46 +0100

    Remove migration scripts

--- backend/src/jobs/data/migration/index.js
+++ backend/src/jobs/data/migration/index.js
@@ -7,9 +7,6 @@ cli.parse(process.argv);
 
 execute(async ({ db }) => {
     let stats = {};
-    stats.unsetPatchCertifInfos = await require('./tasks/unsetPatchCertifInfos')(db);
-    stats.renameCertifInfosAndFormacodes = await require('./tasks/renameCertifInfosAndFormacodes')(db);
     stats.sanitizePhoneNumbers = await require('./tasks/sanitizePhoneNumbers')(db);
-    stats.sanitizeAvisCreated = await require('./tasks/sanitizeAvisCreated')(db);
     return stats;
 });
