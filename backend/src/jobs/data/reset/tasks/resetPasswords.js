7231275610ec544e60b10bfb5e4c78125d161dc2
Benoît Guérout <bguerout@yonoma.com>
2020-01-03 10:52:23 +0100

    Reset data only for last month stagiaires

--- backend/src/jobs/data/reset/tasks/resetPasswords.js
+++ backend/src/jobs/data/reset/tasks/resetPasswords.js
@@ -1,10 +1,15 @@
+const { getNbModifiedDocuments } = require('../../../job-utils');
+
 module.exports = async (db, passwords, password, options = {}) => {
-    return Promise.all([
-        db.collection('accounts').updateMany(options.force ? {} : { passwordHash: { $ne: null } }, {
-            $set: {
-                'meta.rehashed': true,
-                'passwordHash': await passwords.hashPassword(password),
-            }
-        }),
-    ]);
+
+    let res = await db.collection('accounts').updateMany(options.force ? {} : { passwordHash: { $ne: null } }, {
+        $set: {
+            'meta.rehashed': true,
+            'passwordHash': await passwords.hashPassword(password),
+        }
+    });
+
+    return {
+        passwords: getNbModifiedDocuments(res),
+    };
 };
