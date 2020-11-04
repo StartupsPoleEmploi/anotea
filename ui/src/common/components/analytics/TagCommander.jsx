9f48f963746d3c38f41573d4e7a178cf52a2e18b
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-11-04 11:23:12 +0100

    fix tag commander url

--- ui/src/common/components/analytics/TagCommander.jsx
+++ ui/src/common/components/analytics/TagCommander.jsx
@@ -5,9 +5,9 @@ export const initialize = (isProduction = false) => {
 
     const script = document.createElement('script');
     if (!isProduction) {
-      script.src = 'https://cdn.tagcommander.com/5220/uat/tc_zen_31.js';
+      script.src = 'https://cdn.tagcommander.com/5375/uat/tc_Anotea_31.js';
     } else {
-      script.src = 'https://cdn.tagcommander.com/5220/tc_zen_31.js';
+      script.src = 'https://cdn.tagcommander.com/5375/tc_Anotea_31.js';
     }
     script.async = true;
     document.body.appendChild(script);
