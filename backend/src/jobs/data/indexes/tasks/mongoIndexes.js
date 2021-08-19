8c3626378bd2d0c85f3f64fb69988f4e8f55a383
Julien Perrin <julien.perrin01@pole-emploi.fr>
2021-08-19 18:09:54 +0200

    indexer session par id

--- backend/src/jobs/data/indexes/tasks/mongoIndexes.js
+++ backend/src/jobs/data/indexes/tasks/mongoIndexes.js
@@ -32,6 +32,7 @@ module.exports = {
                 'formation.action.session.periode.debut': 1,
                 'formation.action.session.periode.fin': 1,
             }, { name: 'bo-stagiaires-stats' }),
+            db.collection('stagiaires').createIndex({ 'formation.action.session.id': 1 }),
             db.collection('stagiaires').createIndex({ 'formation.action.session.nbStagiaires': 1 }),
         ]);
     },
