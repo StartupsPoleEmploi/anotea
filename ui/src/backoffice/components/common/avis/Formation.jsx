affd1e39d3377defc7e7dcea2d542632c4884b22
Julien Perrin <julien.perrin01@pole-emploi.fr>
2022-12-12 14:20:30 +0100

    fix:ajout notion responsable

--- ui/src/backoffice/components/common/avis/Formation.jsx
+++ ui/src/backoffice/components/common/avis/Formation.jsx
@@ -19,7 +19,7 @@ export default class Formation extends React.Component {
                 <p className="name">{action.organisme_formateur.raison_sociale}&nbsp;&ndash; {action.organisme_formateur.siret}</p>
                 { action?.organisme_formateur?.siret && action?.organisme_responsable?.siret &&
                     action.organisme_formateur.siret.substring(0, 9) !== action.organisme_responsable.siret.substring(0, 9) &&
-                        <p className="name">{action.organisme_responsable.raison_sociale}&nbsp;&ndash; {action.organisme_responsable.siret}</p> }
+                        <p className="name">Responsable&nbsp;: {action.organisme_responsable.raison_sociale}&nbsp;&ndash; {action.organisme_responsable.siret}</p> }
                 <p>
                     <span>{formation.intitule}</span><br />
                     <span>{formation.numero}</span><br />
