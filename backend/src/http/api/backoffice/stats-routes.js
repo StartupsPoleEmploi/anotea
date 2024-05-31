370a8d7d14d2d0d7050d754c82d23a3f013147c5
Elias Khallouk <147084912+EliasKhallouk@users.noreply.github.com>
2024-05-31 10:23:43 +0200

    feat: accessibilite correction date

--- backend/src/http/api/backoffice/stats-routes.js
+++ backend/src/http/api/backoffice/stats-routes.js
@@ -6,13 +6,11 @@ module.exports = ({ db }) => {
     let router = express.Router(); // eslint-disable-line new-cap
     router.get('/api/backoffice/stats', tryAndCatch(async (req, res) => {
         let firstStatsDate = moment('2019-08-01').valueOf();
-        let debutParam = moment('2024-01-30').valueOf();
-        let finParam = moment('2029-06-13').valueOf();
         let now = moment().valueOf();
         let { codeRegion, debut, fin } = await Joi.validate(req.query, {
             codeRegion: Joi.string(),
-            debut: Joi.number().min(debutParam).max(now).default(firstStatsDate),
-            fin: Joi.number().min(finParam).default(now),
+            debut: Joi.number().min(firstStatsDate).max(now).default(firstStatsDate),
+            fin: Joi.number().min(firstStatsDate).default(now),
         }, { abortEarly: false });
 
         let stream = await db.collection('statistics')
