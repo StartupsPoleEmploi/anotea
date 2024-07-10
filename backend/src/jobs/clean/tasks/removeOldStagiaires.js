69e9068380eb3e1be222303dec89999436be41c0
Julien Perrin <julien.perrin01@pole-emploi.fr>
2024-07-10 11:12:43 +0200

    feat: supprimer stagiaries avec avis archives

--- backend/src/jobs/clean/tasks/removeOldStagiaires.js
+++ backend/src/jobs/clean/tasks/removeOldStagiaires.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const { batchCursor } = require('../../job-utils');
 
 module.exports = async (db, logger) => {
     logger.info(`Deleting old stagiaires...`);
@@ -9,31 +10,58 @@ module.exports = async (db, logger) => {
         deletedMailNotSent: 0,
     };
 
-    stats.deletedMailSentNoDate = (await db.collection('stagiaires').updateMany(
+    let cursorDeletedMailSentNoDate = db.collection('stagiaires').find(
         {
             mailSent: true,
             mailSentDate: { $exists: false },
             individu: { $exists: true }
-        },
-        { $unset: { individu: 1 } }
-    )).result.nModified;
+        }
+    );
+    await batchCursor(cursorDeletedMailSentNoDate, async next => {
+        const stagiaire = await next();
+        const res = await db.collection('stagiaires').updateOne(
+            { token: stagiaire.token },
+            { $unset: { individu: 1 } }
+        );
+        if (res.result.nModified > 0) {
+            stats.deletedMailSentNoDate++;
+        }
+    });
 
-    stats.deletedMailSentTooOld = (await db.collection('stagiaires').updateMany(
+    let cursorDeletedMailSentTooOld = db.collection('stagiaires').find(
         {
             mailSent: true,
             mailSentDate: { $lt: moment().subtract(1, 'years').toDate() },
             individu: { $exists: true },
-        },
-        { $unset: { individu: 1 } }
-    )).result.nModified;
+        }
+    );
+    await batchCursor(cursorDeletedMailSentTooOld, async next => {
+        const stagiaire = await next();
+        const res = await db.collection('stagiaires').updateOne(
+            { token: stagiaire.token },
+            { $unset: { individu: 1 } }
+        );
+        if (res.result.nModified > 0) {
+            stats.deletedMailSentTooOld++;
+        }
+    });
 
-    stats.deletedMailNotSent = (await db.collection('stagiaires').updateMany(
+    let cursorDeletedMailNotSent = db.collection('stagiaires').find(
         {
             mailSent: { $ne: true },
             individu: { $exists: true }
-        },
-        { $unset: { individu: 1 } }
-    )).result.nModified;
+        }
+    );
+    await batchCursor(cursorDeletedMailNotSent, async next => {
+        const stagiaire = await next();
+        const res = await db.collection('stagiaires').updateOne(
+            { token: stagiaire.token },
+            { $unset: { individu: 1 } }
+        );
+        if (res.result.nModified > 0) {
+            stats.deletedMailNotSent++;
+        }
+    });
 
     await db.collection('jobs').insertOne({
         type: 'delete-stagiaires',
