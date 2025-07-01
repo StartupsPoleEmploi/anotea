fe3df529d9d5f5c9fe14fc9d469d0154a67e3165
Julien Perrin <JulienPerrin@users.noreply.github.com>
2025-07-01 18:31:53 +0200

    fix: ajout fichier RDS dispositifs de financement

--- backend/src/jobs/data/migration/index.js
+++ backend/src/jobs/data/migration/index.js
@@ -15,7 +15,6 @@ execute(async ({ db, logger }) => {
     };
 
     return {
-        ...(await migrate('fixSiretsAnciensStagiaires')),
         ...(await migrate('fixDispositifsFinancement')),
     };
 });
