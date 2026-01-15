29b8d283463d6c8df56a82436ea68172cc7861a1
Julien Perrin <JulienPerrin@users.noreply.github.com>
2026-01-15 13:45:30 +0100

    fix: filter return api

--- backend/src/http/api/v1/reconcilication/getDocument.js
+++ backend/src/http/api/v1/reconcilication/getDocument.js
@@ -10,6 +10,33 @@ module.exports = (db, type) => async (parameters, options = {}) => {
         { projection: buildProjection(parameters.fields) },
     );
 
+    if (doc?.sessions) {
+        for (sess in doc.sessions) {
+            if (sess.avis) {
+                for (data in sess.avis) {
+                    if (data.commentaire && data.status === 'validated') {
+                        dto.commentaire = {
+                            ...(!_.isEmpty(data.commentaire.text) ? { texte: data.commentaire.text } : {}),
+                            ...(!_.isEmpty(data.commentaire.title) && !data.commentaire.titleMasked ?
+                                { titre: data.commentaire.title } :
+                                {}),
+                        };
+                    } else {
+                        delete dto.commentaire;
+                    }
+                
+                    if (data.reponse && data.status === 'validated' && data.reponse.status === 'validated') {
+                        dto.reponse = {
+                            texte: data.reponse.text,
+                        };
+                    } else {
+                        delete dto.reponse;
+                    }
+                }
+            }
+        }
+    }
+
     if (!doc) {
         throw new IdNotFoundError(`Numéro ${type} inconnu ou ${type} expirée`);
     }
