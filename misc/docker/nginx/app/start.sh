8ea506186d7930036374b7a355caab36670e9fe1
Julien Perrin <JulienPerrin@users.noreply.github.com>
2026-06-15 16:46:24 +0200

    fix nginx restart

--- misc/docker/nginx/app/start.sh
+++ misc/docker/nginx/app/start.sh
@@ -4,6 +4,8 @@ mkdir -p /data/log
 touch /data/log/error.log
 touch /data/log/access.log
 
+rm -f /var/run/nginx.pid
+
 nginx && \
     (fail2ban-client reload || echo "reload not needed" && rm -f /var/run/fail2ban/fail2ban.sock) && \
     fail2ban-server -b && \
